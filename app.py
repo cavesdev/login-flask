d9d3be157c7d026fd0fd74b1759041f72b4f0396
cavesdev <24899540+cavesdev@users.noreply.github.com>
2019-09-03 17:39:14 -0500

    added simple database insertion to /signup

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 
 app = Flask(__name__)
+
+engine = create_engine('postgresql://localhost/login_flask')
+db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
@@ -10,9 +16,22 @@ def index():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        return render_template("login.html")
+    else:
+        return render_template("login.html")
 
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    if request.method == 'POST':
+
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        db.execute('INSERT INTO users (username, password) VALUES (:username, :password)',
+                   {'username': username, 'password': password })
+        db.commit()
+        return render_template('signup.html')
+    else:
+        return render_template("signup.html")
