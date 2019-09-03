d8096b5521823500ac1bea36803ab73205abfc39
cavesdev <24899540+cavesdev@users.noreply.github.com>
2019-09-03 17:46:55 -0500

    added simple database reading for login

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ def index():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        db.execute(f'SELECT * FROM users WHERE username = :username',
+                   {'username': username})
+        db.commit()
         return render_template("login.html")
     else:
         return render_template("login.html")
@@ -32,6 +38,6 @@ def signup():
         db.execute('INSERT INTO users (username, password) VALUES (:username, :password)',
                    {'username': username, 'password': password })
         db.commit()
-        return render_template('signup.html')
+        return render_template('login.html')
     else:
         return render_template("signup.html")
